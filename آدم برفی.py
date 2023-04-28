bfe54dad01875351d7996331e2a722e8efb90797
AmirHossin|Moulodi <132085110+Schoolnew-SeoMaster@users.noreply.github.com>
2023-04-28 20:48:48 +0330

    Add files via upload

---
+++ آدم برفی.py
@@ -0,0 +1,232 @@
+
+import turtle      
+turtle=turtle.Turtle()
+turtle.width(4)
+turtle.hideturtle()
+turtle.color('gray')
+turtle.pensize(5)
+turtle.speed(2)
+
+turtle.penup()     
+turtle.goto(-30,120)
+turtle.pendown()
+turtle.circle(60)
+turtle.begin_fill()
+turtle.circle(-120)
+turtle.end_fill()
+
+turtle.penup()   
+turtle.goto(-50,190)
+turtle.pendown()
+turtle.color('black')
+turtle.begin_fill()
+turtle.circle(5)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-7,190)
+turtle.pendown()
+turtle.color('black')
+turtle.begin_fill()
+turtle.circle(5)
+turtle.end_fill()
+
+
+turtle.penup()  
+turtle.goto(-28,168)
+turtle.pendown()
+turtle.color('darkorange')
+turtle.begin_fill()
+turtle.goto(-55,168)
+turtle.goto(-28,180)
+turtle.goto(-28,168)
+turtle.end_fill()
+
+
+turtle.penup()    
+turtle.goto(-40,150)
+turtle.pendown()
+turtle.color('lime')
+turtle.pensize(6)
+turtle.forward(20)
+
+
+turtle.penup()    
+turtle.goto(62,80)
+turtle.left(30)
+turtle.pendown()
+turtle.color('gray')
+turtle.begin_fill()
+for i in range(2): 
+    turtle.forward(80)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.penup()          
+turtle.goto(-142,50)
+turtle.left(120)
+turtle.pendown()
+turtle.color('gray')
+turtle.begin_fill()
+for i in range(2):
+    turtle.forward(80)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.begin_fill()   
+turtle.penup()
+turtle.goto(145,122)
+turtle.pendown()
+turtle.color('darkblue')
+turtle.circle(10)
+turtle.end_fill()
+
+turtle.begin_fill()   
+turtle.penup()
+turtle.goto(-200,108)
+turtle.pendown()
+turtle.color('darkblue')
+turtle.circle(10)
+turtle.end_fill()
+
+turtle.color('black')  
+turtle.begin_fill()
+turtle.penup()
+turtle.goto(-28,65)
+turtle.pendown()
+turtle.circle(5)
+turtle.end_fill()
+
+turtle.color('black')  
+turtle.begin_fill()
+turtle.penup()
+turtle.goto(-28,15)
+turtle.pendown()
+turtle.circle(5)
+turtle.end_fill()
+
+turtle.color('black')  
+turtle.begin_fill()
+turtle.penup()
+turtle.goto(-28,-35)
+turtle.pendown()
+turtle.circle(5)
+turtle.end_fill()
+
+
+turtle.penup()    
+turtle.goto(40,-100)
+turtle.left(122)
+turtle.pendown()
+turtle.color('gray')
+turtle.begin_fill()
+for i in range(2):
+    turtle.forward(65)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.penup()    
+turtle.goto(-160,-135)
+turtle.left(150)
+turtle.pendown()
+turtle.color('gray')
+turtle.begin_fill()
+for i in range(2):
+    turtle.forward(68)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.penup()    
+turtle.goto(68,-185)
+turtle.left(122)
+turtle.pendown()
+turtle.color('gold')
+turtle.begin_fill()
+for i in range(2):
+    turtle.forward(55)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+turtle.end_fill()
+
+
+turtle.penup()    
+turtle.goto(-182,-125)
+turtle.left(150)
+turtle.pendown()
+turtle.color('gold')
+turtle.begin_fill()
+for i in range(2):
+    turtle.forward(55)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+    
+turtle.end_fill()
+turtle.penup()      
+turtle.goto(32,210)
+turtle.pendown()
+turtle.color('limegreen')
+turtle.begin_fill()
+turtle.goto(-90,210)
+turtle.goto(-30,325)
+turtle.goto(32,210)
+turtle.end_fill()
+
+turtle.penup()    
+turtle.goto(140,112)
+turtle.left(-70)
+turtle.pendown()
+turtle.color('brown')
+turtle.pensize(5)
+turtle.forward(260)
+
+turtle.penup()    
+turtle.goto(-205,100)
+turtle.left(-10)
+turtle.pendown()
+turtle.color('brown')
+turtle.pensize(5)
+turtle.forward(260)
+
+turtle.penup()   
+turtle.goto(-200,-260)
+turtle.pendown()
+turtle.pencolor('magenta')
+turtle.write('مولودي',font=('b titr',20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
